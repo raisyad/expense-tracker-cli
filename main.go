1629d5918d6a107dfe6a275b06c0c5b001129798
raisyadjullfikar <raisyadjullfikar@upi.edu>
2026-03-10 00:36:17 +0700

    update: step for choice input dynamic with for and if statements

--- main.go
+++ main.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
+func showMenus() {
+	fmt.Println("=== Expense Tracker CLI ===")
+	fmt.Println("1. Tambah Pengeluaran")
+	fmt.Println("2. Lihat Pengeluaran")
+	fmt.Println("3. Hapus Pengeluaran")
+	fmt.Println("4. Keluar")
+	fmt.Print("Pilih menu: ")
+}
+
 func main() {
-	fmt.Println("Hallo")
+	var choice int
+
+	for {
+		showMenus()
+		fmt.Scan(&choice)
+		switch choice {
+		case 1:
+			fmt.Println("Tambah Pengeluaran")
+		case 2:
+			fmt.Println("Lihat Pengeluaran")
+		case 3:
+			fmt.Println("Hapus Pengeluaran")
+		case 4:
+			fmt.Println("Keluar")
+			return
+		default:
+			fmt.Println("Pilihan tidak valid")
+		}
+	}
 }
